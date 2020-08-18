ed4449f3bdd40051742691badd7628914ea7194f
lubenic4 <zinedin.mezit@edu.fit.ba>
2020-08-18 12:42:41 +0200

    migrations

--- fandom.WebAPI/Migrations/20200818104206_180820_RemoveTables.Designer.cs
+++ fandom.WebAPI/Migrations/20200818104206_180820_RemoveTables.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fandom.WebAPI.Database;
 
 namespace fandom.WebAPI.Migrations
 {
     [DbContext(typeof(AppCtx))]
-    partial class AppCtxModelSnapshot : ModelSnapshot
+    [Migration("20200818104206_180820_RemoveTables")]
+    partial class _180820_RemoveTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
